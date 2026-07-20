b51046ff3702693ad53a63e4d895e19c9cab8f34
Mohammed Essam <projectapp403@gmail.com>
2026-07-20 21:02:04 +0300

    Update CustomTextField.dart

--- lib/widgets/CustomTextField.dart
+++ lib/widgets/CustomTextField.dart
@@ -1,38 +1,42 @@
+/* 
+====================================================
+===========(START CUSTOM TEXT FIELD)================
+====================================================
+*/
+
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final String hint;
   final IconData icon;
   final bool obscure;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
 
   const CustomTextField({
     super.key,
     required this.hint,
     required this.icon,
     this.obscure = false,
+    this.controller,
+    this.validator,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      controller: controller,
       obscureText: obscure,
+      validator: validator,
       decoration: InputDecoration(
         prefixIcon: Icon(icon),
-
-        suffixIcon:
-            obscure
-                ? const Icon(Icons.visibility_outlined)
-                : null,
-
+        suffixIcon: obscure ? const Icon(Icons.visibility_outlined) : null,
         hintText: hint,
-
         filled: true,
         fillColor: Colors.white,
-
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(18),
         ),
-
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(18),
           borderSide: BorderSide(color: Colors.grey.shade300),
